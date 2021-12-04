b8ce1da58becd43bb5267473fcf7d4fdf1ea0f87
JaxC <90055896+JacquelineCL@users.noreply.github.com>
2021-12-04 13:41:37 -0500

    Starting Challenge

--- static/js/app.js
+++ static/js/app.js
@@ -41,32 +41,3 @@ d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
-
-// // Takes two numbers and adds them
-// function addition(a, b) {
-//     return a + b;
-//   }
-// // Functions can call other functions
-// function doubleAddition(c, d) {
-//     var total = addition(c, d) * 2;
-//     return total;
-//   }
-
-// console.log(doubleAddition(65,34));
-
-// let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
-// console.log(friends);
-
-// function listLoop(userList) {
-//   for (var i = 0; i < userList.length; i++) {
-//     console.log(userList[i]);
-//   }
-// }
-
-// listLoop(friends);
-
-// let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
-
-// for (var i = 0; i < vegetables.length; i++) {
-//   console.log("I love " + vegetables[i]);
-// }
