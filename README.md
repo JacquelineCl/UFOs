6a91b111f69b4b43f068e9e02346737ab6d69f46
JaxC <90055896+JacquelineCL@users.noreply.github.com>
2021-12-05 20:47:00 -0500

    challenge complete

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # UFOs
 module 11
+## Overview
+This project was completed to make searching through reported UFO sightings easier by making the information easier to access. 
+
+## Results
+The ability to view the webpage on multiple device types. Plus, the ability to filter the ufo sightings by date, city, state, country, and/or shape. Examples of expected formatting can be found in each search box when the page first loads. 
+
+In order to get to the information you're looking for:
+1. Click on the appropriate input box and enter a specific date/city/state/country/shape
+2. Click enter
+3. The table on the right will update based upon the information entered
+4. Enter additional search criteria in another input box to further refine the results if desired
+
+If you don't receive the expected results, verify the formatting of the inforamtion matches the image below:
+image url
+
+## Summary
+This webpage does simplify searching through the information provided much easier, however it does  
+
+Further development could include:
+* adding a way to easily clear the filters
+* adding the ability to easily export the table
+* automatically updating the information if a reliable source is found
--- static/js/app.js
+++ static/js/app.js
@@ -8,14 +8,12 @@ function buildTable(data) {
   // First, clear out any existing data
   tbody.html("");
 
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
+  // Next, loop through each object in the data and append a row and cells for each value in the row
   data.forEach((dataRow) => {
     // Append a row to the table body
     let row = tbody.append("tr");
 
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
+    // Loop through each field in the dataRow and add each value as a table cell (td)
     Object.values(dataRow).forEach((val) => {
       let cell = row.append("td");
       cell.text(val);
@@ -23,44 +21,64 @@ function buildTable(data) {
   });
 }
 
-// 1. Create a variable to keep track of all the filters as an object.
+var filter = {
+  datetime: null,
+  city: null,
+  state: null,
+  country: null,
+  shape: null,
+  color: null
+};
 
-
-// 3. Use this function to update the filters. 
-function updateFilters() {
-
-    // 4a. Save the element that was changed as a variable.
-
-    // 4b. Save the value that was changed as a variable.
-
-    // 4c. Save the id of the filter that was changed as a variable.
-
-  
-    // 5. If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object.
- 
-  
-    // 6. Call function to apply all filters and rebuild the table
-    filterTable();
-  
+function updateFilters(filterType, filterId) {
+  filter[filterType] = d3.select(filterId).property('value');
+  filterTable();
   }
   
-  // 7. Use this function to filter the table when data is entered.
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData;
   
-    // 9. Loop through all of the filters and keep any data that
-    // matches the filter values
-    
+    // 9. Loop through all of the filters and keep any data that matches the filter values
+    filteredData = filteredData.filter(row => {
+      
+      if (filter.datetime != null && filter.datetime != row.datetime){
+        return false;
+      } if (filter.city != null && filter.city != row.city){
+          return false; 
+        } if (filter.state != null && filter.state != row.state){
+            return false;
+          } if (filter.country != null && filter.country != row.country){
+              return false;
+            } if (filter.shape != null && filter.shape != row.shape){
+                return false;
+              } return true;
+    });
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData);
   }
   
+
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.select("#datetime").on("change", function(){
+    updateFilters('datetime', '#datetime');
+  });
+  d3.select("#city").on("change", function(){
+    updateFilters('city', '#city');
+  });
+  d3.select("#state").on("change", function(){
+    updateFilters('state', '#state');
+  });
+  d3.select("#country").on("change", function(){
+    updateFilters('country', '#country');
+  });
+  d3.select("#shape").on("change", function(){
+    updateFilters('shape', '#shape');
+  });
+
+
   
   // Build the table when the page loads
   buildTable(tableData);
